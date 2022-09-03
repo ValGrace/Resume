0004071c1d221e9f7e65151d8e6d114aa974f73f
ValGrace <gracevalerie1@gmail.com>
2022-09-03 15:30:54 +0300

    feat: create react app

---
+++ resume-app/src/App.js
@@ -0,0 +1,14 @@
+import  Header from './Components/Header'
+import ResumeBody from './Components/ResumeBody';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ResumeBody />
+    </div>
+  );
+}
+
+export default App;
